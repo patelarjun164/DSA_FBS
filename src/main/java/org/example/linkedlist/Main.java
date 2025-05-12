a10bcb7f3f7239741475b719efaa688c390fdcfc
Arjun Patel <patelarjun164@gmail.com>
2025-05-12 20:53:27 +0530

    singly linked list code

--- src/main/java/org/example/linkedlist/Main.java
+++ src/main/java/org/example/linkedlist/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.linkedlist;
 
 public class Main {
     Node head = null;
--- src/main/java/org/example/linkedlist/Node.java
+++ src/main/java/org/example/linkedlist/Node.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.linkedlist;
 
 public class Node {
     String data;
