5f3166641dde2bae17d6cc520f757d735219974d
Arjun Patel <patelarjun164@gmail.com>
2025-05-13 20:05:33 +0530

    delete added

--- src/main/java/org/example/linkedlist/Main.java
+++ src/main/java/org/example/linkedlist/Main.java
@@ -4,7 +4,7 @@ public class Main {
     Node head = null;
     public static void main(String[] args) {
         Node n1 = new Node("Arjun");
-        Node n2 = new Node("Jiya Pagal");
+        Node n2 = new Node("Hello");
         Node n3 = new Node("Chetansir");
 
         Main app = new Main();
@@ -13,6 +13,37 @@ public class Main {
         app.insert(n3);
 
         app.display();
+//        app.search("Hello");
+        app.delete("Chetansir");
+        app.display();
+    }
+
+    private void delete(String str) {
+        Node current = head;
+        if(head.data.equals(str)){
+            head = head.next;
+        } else {
+            while(!current.next.data.equals(str)){
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+    }
+
+    private boolean search(String str) {
+        Node current = head;
+        Node locationRef;
+        while(current != null){
+            System.out.println(current.data);
+            if(current.data.equals(str)){
+                locationRef = current;
+                System.out.println("Available");
+                return true;
+            }
+            current = current.next;
+        }
+        System.out.println("Not Available");
+        return false;
     }
 
     private void display() {
