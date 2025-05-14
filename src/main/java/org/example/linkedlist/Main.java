301a2fae804380fa18e459089e0ac7948476b62c
Arjun Patel <patelarjun164@gmail.com>
2025-05-14 19:49:16 +0530

    insert at pos

--- src/main/java/org/example/linkedlist/Main.java
+++ src/main/java/org/example/linkedlist/Main.java
@@ -2,28 +2,34 @@ package org.example.linkedlist;
 
 public class Main {
     Node head = null;
+
     public static void main(String[] args) {
         Node n1 = new Node("Arjun");
-        Node n2 = new Node("Hello");
+        Node n2 = new Node("Dhanashri");
         Node n3 = new Node("Chetansir");
 
         Main app = new Main();
+
         app.insert(n1);
         app.insert(n2);
         app.insert(n3);
+        System.out.println("Before ----");
+        app.display();
+        app.insert(new Node("myjio"), 2);
+        System.out.println("After ----");
+        app.display();
 
-        app.display();
 //        app.search("Hello");
-        app.delete("Chetansir");
-        app.display();
+//        app.delete("Chetansir");
+//        app.display();
     }
 
     private void delete(String str) {
         Node current = head;
-        if(head.data.equals(str)){
+        if (head.data.equals(str)) {
             head = head.next;
         } else {
-            while(!current.next.data.equals(str)){
+            while (!current.next.data.equals(str)) {
                 current = current.next;
             }
             current.next = current.next.next;
@@ -33,9 +39,9 @@ public class Main {
     private boolean search(String str) {
         Node current = head;
         Node locationRef;
-        while(current != null){
+        while (current != null) {
             System.out.println(current.data);
-            if(current.data.equals(str)){
+            if (current.data.equals(str)) {
                 locationRef = current;
                 System.out.println("Available");
                 return true;
@@ -48,20 +54,37 @@ public class Main {
 
     private void display() {
         Node current = head;
-        while(current != null){
+        while (current != null) {
             System.out.println(current.data);
             current = current.next;
         }
     }
 
     private void insert(Node ref) {
-        if(head==null){
+        if (head == null) {
             head = ref;
         } else {
             Node current = head;
-            while(current.next != null){
+            while (current.next != null) {
                 current = current.next;
             }
+            current.next = ref;
+        }
+    }
+
+    public void insert(Node ref, int position) {
+        if (position == 1) {
+            ref.next = head;
+            head = ref;
+        } else {
+            Node current = head;
+            int currentPos = 1;
+            while (currentPos < position - 1) {
+                current = current.next;
+                currentPos++;
+            }
+
+            ref.next = current.next;
             current.next = ref;
         }
     }
